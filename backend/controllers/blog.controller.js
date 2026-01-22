2e2ec4d079f5c84d9d3313fccae4ca95a3563057
rajesh-rana01 <rajeshra5naa@gmail.com>
2026-01-22 10:43:30 +0545

    get autherblog, update , delete

--- backend/controllers/blog.controller.js
+++ backend/controllers/blog.controller.js
@@ -1,3 +1,4 @@
+import { populate } from "dotenv";
 import Blog from "../models/blog.model.js";
 
 export const createBlog = async(req, res)=>{
@@ -44,6 +45,81 @@ export const getAllBlogs = async(req, res)=>{
     }
 }
 
-export const blogsOfAuther = async(req, res)=>{
-    
+export const getAutherBlogs = async(req, res)=>{
+    try {
+        const autherId = req.user.userId; // from authenticated user (authentication middleware)
+        const autherBlogs = await Blog.findOne({auther:autherId})
+        //to get data related
+        .populate('auther','fullName email')
+        .sort({createdAt:-1})
+
+        return res.status(200).json({
+            message:"Sucessfully got auther blogs !",
+            count:autherBlogs.length,
+            autherBlogs
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message:`Get auther blogs error: ${error}`
+        })
+    }
+}
+
+export const updateBlog = async(req, res)=>{
+    try {
+        const {title, content, category} = req.body;
+        const blogId = req.params.blogId;
+        const autherId = req.user.userId; 
+
+        const blog = await Blog.findOne({
+            _id:blogId,
+            auther:autherId
+        });
+        if(!blog){
+            return res.status(404).json({message:"Blog not found or you are not authorized !"});
+        }
+        //update fields 
+        if(title) blog.title = title;
+        if(content) blog.content = content;
+        if(category) blog.category = category;
+
+        await blog.save();
+
+        await blog.populate('auther','fullName email')
+
+        return res.status(200).json({
+            message:"Blog updated sucessfully !",
+            blog
+        })
+
+    } catch (error) {
+        return res.status(500).json({message:`Update blog error: ${error}`})
+    }
+}
+
+export const deleteBlog = async(req, res)=>{
+    try {
+        const blogId = req.params.blogId;
+        const autherId = req.user.userId;
+
+        const blog = await Blog.findOne({
+            _id:blogId,
+            auther:autherId
+        });
+        if(!blog){
+            return res.status(404).json({
+                message:"Blog not found or you are not authorized !"
+            });
+        }
+        await Blog.deleteOne({
+            _id:blogId,
+            auther:autherId
+        })
+        return res.status(200).json({
+            message:"Blog deleted sucessfully !",
+            blog
+        })
+    } catch (error) {
+        return res.status(500).json({message:`Delete blog error: ${error}`});
+    }
 }
--- backend/routes/blog.route.js
+++ backend/routes/blog.route.js
@@ -1,10 +1,13 @@
 import express from 'express'
-import { createBlog, getAllBlogs } from '../controllers/blog.controller.js';
+import { createBlog, deleteBlog, getAllBlogs, getAutherBlogs, updateBlog } from '../controllers/blog.controller.js';
 import authenticateToken from '../middlewares/authenticateToken.js';
 
 const blogRouter = express.Router(); 
 
 blogRouter.post('/create-blog',authenticateToken,createBlog)
-blogRouter.get('/get-all-blogs', getAllBlogs)
+blogRouter.get('/get-all-blogs',authenticateToken ,getAllBlogs)
+blogRouter.get('/get-auther-blogs', authenticateToken, getAutherBlogs)
+blogRouter.put('/update-blog/:blogId', authenticateToken, updateBlog)
+blogRouter.delete('/delete-blog/:blogId', authenticateToken, deleteBlog)
 
 export default blogRouter;
