b35211c2dce69527833aa1e0cf8558a798b065e0
rajesh-rana01 <rajeshra5naa@gmail.com>
2026-01-25 13:50:17 +0545

    fix type error

--- backend/middlewares/validation.js
+++ backend/middlewares/validation.js
@@ -5,7 +5,7 @@ export const validation= (schema)=>(req, res, next)=>{
         schema.parse(req.body);
         next()
     } catch (err) {
-        if(err instanceof zodError){
+        if(err instanceof ZodError){
             return res.status(400).json({
                 errors: err.errors.map(e=>e.message)
             })
